01dce2694f44e1fe136d1804753034b3b381cdee
vidguru1008@gmail.com <@Oranges1974>
2021-11-24 16:41:19 +0530

    first commit

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,6 +3,6 @@ package org.launchcode;
 public class Main {
     //Main test
     public static void main(String[] args) {
-    System.out.println("Main Pgm...")
+    System.out.println("Main Pgm...");
     }
 }
